28fb69bb599f84c22695d43ca9d4b9934ca4b51d
leomoyano <leomoyano7@gmail.com>
2021-08-22 02:35:49 -0300

    update 21/08

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import { CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
 const List = () => {
     const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
     const classes = useStyles();
+
+    const places = [
+        {name: "Cool Place"},
+        {name: "Best Beer"},
+        {name: "Best Steak"},
+    ];
 
     return (
         <div className={classes.container}>
@@ -20,6 +28,22 @@ const List = () => {
                     <MenuItem value="attractions">Attractions</MenuItem>
                 </Select>
             </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place}/>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
